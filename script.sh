0f5de283594a8c0d64d48e278a86140be83ccd8d
mikhail.shergin <mikshergin@gmail.com>
2023-05-12 18:04:47 +0300

    Add clean-results option

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ ALLURE_RESULTS_DIRECTORY=$1
 PROJECT_ID=$2
 IS_SECURE=$3
 ALLURE_GENERATE=$4
+ALLURE_CLEAN_RESULTS=$5
 
 SECURITY_USER=$ALLURE_SERVER_USER
 SECURITY_PASS=$ALLURE_SERVER_PASSWORD
@@ -54,30 +55,42 @@ if [[ "$IS_SECURE" == "true" ]]; then
     CRSF_ACCESS_TOKEN_VALUE=$(cat cookiesFile | grep -o 'csrf_access_token.*' | cut -f2)
     echo "done"
     # echo "csrf_access_token value: $CRSF_ACCESS_TOKEN_VALUE"
-    
-    echo "------------------SEND-RESULTS------------------"
+fi
+
+if [[ "$ALLURE_CLEAN_RESULTS" == "true" ]]; then
+    echo "------------------CLEAN-RESULTS------------------"
+    CLEAN_RESULTS_URL="$ALLURE_SERVER/allure-docker-service/clean-results?project_id=$PROJECT_ID"
+    if [[ "$IS_SECURE" == "true" ]]; then
+        RESPONSE=$(curl -X GET "$CLEAN_RESULTS_URL" -H "X-CSRF-TOKEN: $CRSF_ACCESS_TOKEN_VALUE" -b cookiesFile)
+    else
+        RESPONSE=$(curl -X GET "$CLEAN_RESULTS_URL")
+    fi
+    echo "done clean results"
+fi
+
+echo "------------------SEND-RESULTS------------------"
+if [[ "$IS_SECURE" == "true" ]]; then
     curl -X POST "$ALLURE_SERVER/allure-docker-service/send-results?project_id=$PROJECT_ID" \
     -H 'Content-Type: multipart/form-data' \
     -H "X-CSRF-TOKEN: $CRSF_ACCESS_TOKEN_VALUE" \
     -b cookiesFile $FILES -ik
-    echo "done"
     set -o xtrace
 else
-    echo "------------------SEND-RESULTS------------------"
     curl -X POST "$ALLURE_SERVER/allure-docker-service/send-results?project_id=$PROJECT_ID" -H 'Content-Type: multipart/form-data' $FILES -ik
 fi
+echo "done send-results"
 
 if [[ "$ALLURE_GENERATE" == "true" ]]; then
     echo "-----------------GENERATE-REPORT----------------"
     EXECUTION_NAME='GitHub+Actions'
     EXECUTION_FROM="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
-    EXECUTION_FROM_ENCODED=="$(curl -s -o /dev/null -w %{url_effective} --get --data-urlencode "$EXECUTION_FROM" "" | cut -c 3-)"
+    EXECUTION_FROM_ENCODED="$(curl -s -o /dev/null -w %{url_effective} --get --data-urlencode "$EXECUTION_FROM" "" | cut -c 3-)"
 
     GENERATE_URL="$ALLURE_SERVER/allure-docker-service/generate-report?project_id=$PROJECT_ID&execution_name=$EXECUTION_NAME&execution_from=$EXECUTION_FROM_ENCODED"
     if [[ "$IS_SECURE" == "true" ]]; then
-        RESPONSE=$(curl -X GET "$GENERATE_URL" -H "X-CSRF-TOKEN: $CRSF_ACCESS_TOKEN_VALUE" -b cookiesFile $FILES)
+        RESPONSE=$(curl -X GET "$GENERATE_URL" -H "X-CSRF-TOKEN: $CRSF_ACCESS_TOKEN_VALUE" -b cookiesFile)
     else
-        RESPONSE=$(curl -X GET "$GENERATE_URL" $FILES)
+        RESPONSE=$(curl -X GET "$GENERATE_URL")
     fi
     echo $(grep -o '"report_url":"[^"]*' <<< "$RESPONSE" | grep -o '[^"]*$')
 fi
